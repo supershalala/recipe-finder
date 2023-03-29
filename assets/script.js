7e133690866f5336a5348caf29488f072f35a71b
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-03-29 15:28:40 +1100

    formatting on nav bar resizing

--- assets/script.js
+++ assets/script.js
@@ -326,3 +326,11 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  // JS mediaquery for navDiv
+
+  $(window).resize(function() {
+    if (window.innerWidth <= 1000) $("#navDiv").addClass('pb-6');
+    else $homeIcon.removeClass('pb-6');
+  });
+
